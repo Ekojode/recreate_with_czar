badad95fdf3f3ec3570e9f894eac3568beb38313
Ekojode Oma-Victor <ekojodeoma@gmail.com>
2024-01-10 15:14:15 +0100

    Set up basic localization
    Set up basic localization of the application

---
+++ lib/utilities/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0XFF650C01);
+  static const background = Color(0XFFDEDECE);
+  static const white = Colors.white;
+  static const black = Colors.black;
+}
